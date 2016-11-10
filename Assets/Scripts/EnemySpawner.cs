046e94b8748e375528b6e9f9a66adbdddb13c373
tdvance <tdvance@gmail.com>
2016-11-09 21:31:51 -0500

    Spawn (not yet moving) enemy formation

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+    public GameObject[] enemyPrefabs;
+
+    // Use this for initialization
+    void Start() {
+        SpawnFormation();
+       
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void SpawnFormation() {
+        int which = 0;
+        bool second = true;
+        foreach (Transform child in transform) {
+            SpawnEnemy(child, which);
+            if (second) {
+                second = false;
+                which++;
+            } else {
+                second = true;
+            }
+        }
+    }
+
+    GameObject SpawnEnemy(Transform parent, int which) {
+        GameObject enemy = Instantiate(enemyPrefabs[which], new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+        enemy.transform.SetParent(parent, false);
+
+        return enemy;
+    }
+}
