a684ac4f5efe7c0e06450559bf1eca524dbf02d1
tdvance <tdvance@gmail.com>
2016-11-11 09:47:13 -0500

    Enemies fire at player player has health enemies kill player when health is zero

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
     public float health = 2f;
+    public GameObject enemyBoltPrefab;
+    public float boltVelocity = 20f;
+    public float firingRate = 0.5f;
 
     // Use this for initialization
     void Start() {
@@ -11,13 +14,15 @@ public class Enemy : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-
+        if (Random.value < firingRate * Time.deltaTime) {//TODO increase with skill level or fewer ships
+            Fire();
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collider) {
         if (collider.tag == "FriendlyFire") {
             Hit(collider.gameObject.GetComponent<Bolt>().damage);
-            Destroy(collider.gameObject);
+            Destroy(collider.gameObject);//TODO spawn something?
         }
     }
 
@@ -25,6 +30,15 @@ public class Enemy : MonoBehaviour {
         health -= damage;
         if (health <= 0) {
             Destroy(gameObject);
+            //TODO Spawn smoke
+        } else {
+            //TODO show damage
         }
+    }
+
+    void Fire() {
+        //TODO alt-fire, combo fire
+        GameObject bolt = Instantiate(enemyBoltPrefab, new Vector3(transform.position.x, transform.position.y - 0.5f), Quaternion.identity) as GameObject;
+        bolt.GetComponent<Rigidbody2D>().velocity = Vector2.down * boltVelocity;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,11 @@ public class PlayerController : MonoBehaviour {
     public float xMin = -8.4f;
     public float xMax = 8.4f;
 
+    public float health = 3f;
     public GameObject laserBoltPrefab;
     public float laserboltY = -3.86f;
     public float boltVelocity = 20f;
+
 
     // Use this for initialization
     void Start() {
@@ -30,6 +32,14 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.tag == "EnemyFire") {
+            Hit(collider.gameObject.GetComponent<Bolt>().damage);
+            Destroy(collider.gameObject);//TODO spawn something?
+        }
+    }
+
+
     float GetMovementInput() {
         return CrossPlatformInputManager.GetAxis("Horizontal") * speed;
     }
@@ -44,8 +54,19 @@ public class PlayerController : MonoBehaviour {
     }
 
     void Fire() {
+        //TODO alt-fire, combo fire
         GameObject bolt = Instantiate(laserBoltPrefab, new Vector3(transform.position.x, laserboltY), Quaternion.identity) as GameObject;
         bolt.GetComponent<Rigidbody2D>().velocity = Vector2.up * boltVelocity;
+    }
+
+    void Hit(float damage) {
+        health -= damage;
+        if (health <= 0) {
+            Destroy(gameObject);
+            //TODO Spawn smoke
+        } else {
+            //TODO show damage
+        }
     }
 
 }
