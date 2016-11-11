c54556a4f6430709bd9ec7db4c7a83dcad2458a8
tdvance <tdvance@gmail.com>
2016-11-11 09:03:23 -0500

    Friendly fire now destroys enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        Debug.Log("Hit!");
+
+        if (collider.tag == "FriendlyFire") {
+            Destroy(gameObject);
+            Destroy(collider.gameObject);
+        }
+    }
+}
